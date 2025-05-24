5a46d395bae39df33f83a51eea686ffe883d9d43
Leïa <129400344+Leia-Delafontaine@users.noreply.github.com>
2025-05-24 23:46:07 +0200

    Create PythagoreanTheorem.py
    A Pythagorean theorem is the relationship between the three sides of a right triangle.
    
    Let's create a little program that asks the user for two numbers, a and b, and then calculates the hypotenuse c.

---
+++ PythagoreanTheorem.py
@@ -0,0 +1,7 @@
+# It's  a  program that asks the user for two numbers, a and b, and then calculates the hypotenuse c.
+
+a = int(input('enter a :'))  # the length of the short side
+b = int(input('enter b :'))  # the length of the other short side
+c=((a ** 2)+(b ** 2))** 0.5  
+
+print(c)
