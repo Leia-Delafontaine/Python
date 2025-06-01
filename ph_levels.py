841df885404b638de74001eb35ddcf35b282733a
Leïa <129400344+Leia-Delafontaine@users.noreply.github.com>
2025-06-01 20:18:51 +0200

    Create ph_levels.py

---
+++ ph_levels.py
@@ -0,0 +1,11 @@
+# A program that checks whether a pH level is basic, acidic, or neutral.
+
+ph = 10
+if ph > 7:
+  print ('Basic')
+elif ph < 7:
+  print ('Acidic')
+else :
+  print ('Neutral')
+
+# Elif : One or more elif statements (short for "else if") can be optionally added in between the if and else to provide additional condition(s) to check. Sometimes two is simply not enough.
