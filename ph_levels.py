cded48ec904f4fe3a115b64695102ae03cfe7962
Leïa <129400344+Leia-Delafontaine@users.noreply.github.com>
2025-06-01 21:36:31 +0200

    Update ph_levels.py

--- ph_levels.py
+++ ph_levels.py
@@ -5,7 +5,7 @@ if ph > 7:
   print ('Basic')
 elif ph < 7:
   print ('Acidic')
-else :
+else:
   print ('Neutral')
 
 # Elif : One or more elif statements (short for "else if") can be optionally added in between the if and else to provide additional condition(s) to check. Sometimes two is simply not enough.
